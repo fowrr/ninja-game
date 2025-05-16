b25efa7c58272c80a679598a39011f08ad877ac9
fowrr <smand3539@gmail.com>
2025-05-16 16:29:08 -0400

    ok im not fricking this up again this time

--- bean.gd
+++ bean.gd
@@ -1,14 +1,25 @@
 extends RigidBody3D
 signal veloSpeed
-var velocity := 1000.0
+signal colF
+signal colT
+var velocity := 2500.0
 var mouse_sens = 0.3
 @onready var hrzn = $v
 @onready var vert = $v/h
 @onready var mNode = $movementNode
 @onready var velocity_label = $"Control/velocity"
 var grounded = 0
-var max_horizontal_speed := 4.0
+var max_horizontal_speed := 2.0
+var max_diagonal_speed := 2.82842712475
 var jumps = 2
+@onready var rayCast = $v/h/S/Camera3D/RayCast3D
+@onready var h = ($"v/h/S/Camera3D/RayCast3D/rayball").get_surface_override_material(0)
+@onready var ball = $v/h/S/Camera3D/RayCast3D/rayball
+@onready var rayEnd = $v/h/S/Camera3D/longRay/rayEnd
+@onready var longRay = $v/h/S/Camera3D/longRay
+var colliding = false
+var launch = false
+var launchWas = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -17,43 +28,102 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	print(grounded)
-	
-	
-	var speed = get_linear_velocity()
-	if grounded == 1:
-		if abs(linear_velocity.x) > max_horizontal_speed:
-			linear_velocity.x = sign(linear_velocity.x) * max_horizontal_speed
-		if abs(linear_velocity.z) > max_horizontal_speed:
-			linear_velocity.z = sign(linear_velocity.z) * max_horizontal_speed
-	elif grounded == 0:
-		velocity = 500
-	veloSpeed.emit(speed)
-	velocity_label.text = "Velocity: " + str(speed)
-	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	var horizon_basis = mNode.basis
-	apply_central_force(Vector3(input.x, 0 ,input.y) * velocity * 1 * delta * horizon_basis)
-	if Input.is_action_pressed("shift") and grounded == 1:
+	var horizontal_velocity = Vector2(linear_velocity.x, linear_velocity.z)
+	#print(rayCast.transform.basis)
+	#Code making sure my velocity is never higher than my max speed.
+	#===============================================================#
+	var current_velocity = linear_velocity # Get the current velocity of the rigid body
+	if launch == true and launchWas == true:
+		velocity = 2500
+		max_horizontal_speed = 10.0
+		if horizontal_velocity.length()> 11:
+			horizontal_velocity = horizontal_velocity.normalized() * max_horizontal_speed
+			linear_velocity = Vector3(horizontal_velocity.x,linear_velocity.y,horizontal_velocity.y)
+		#print(current_velocity.length())
+	elif Input.is_action_pressed("shift") and grounded == 1:
 		velocity = 2500
 		max_horizontal_speed = 6.0
+		if horizontal_velocity.length()> 7:
+			horizontal_velocity = horizontal_velocity.normalized() * max_horizontal_speed
+			linear_velocity = Vector3(horizontal_velocity.x,linear_velocity.y,horizontal_velocity.y) #This code make sure my speed is capped if I'm sprinting
+		#print(current_velocity.length())
 	else:
-		velocity = 1000
-		max_horizontal_speed = 3.0
+		max_horizontal_speed = 2.0
+		if horizontal_velocity.length()> 3: #Same gist as the code above, just now it's when my speed is normal (i.e. not sprinting).
+			horizontal_velocity = horizontal_velocity.normalized() * max_horizontal_speed
+			linear_velocity = Vector3(horizontal_velocity.x,linear_velocity.y,horizontal_velocity.y)
+	#print(current_velocity.length())
+	if linear_velocity.y <= -1.5:
+		gravity_scale = 1
+	else:
+		gravity_scale = 1
+		
+	
+	#Label for velocity
+	veloSpeed.emit(current_velocity)
+	velocity_label.text = "Velocity: " + str(current_velocity)
+	
+	#Gets my movement (A,W,S,D or arrow keys)
+	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var horizon_basis = mNode.basis #Check for nMode explanation (second line in unhandled input).
+	#horizon_basis = look_at(grapple_point)
+	apply_central_force(Vector3(input.x, 0 ,input.y) * velocity * 1 * delta * horizon_basis)
+	
+	#hi
+	col()
+	
+func col():
+	if longRay.is_colliding():
+		if rayCast.is_colliding():
+			h.albedo_color = Color(0,1,0)
+			var collision_point = rayCast.get_collision_point()
+			ball.global_transform.origin = collision_point
+			colliding = true
+			emit_signal("colT")
+		else:
+			h.albedo_color = Color(1,0,0)
+			var longRayCol = longRay.get_collision_point()
+			ball.global_transform.origin = longRayCol
+			colliding = false
+			emit_signal("colF")
+	else:
+		h.albedo_color = Color(1,0,0)
+		ball.global_transform.origin = rayEnd.global_transform.origin
+		colliding = false
+		emit_signal("colF")
+		return colliding
+
+
 func _unhandled_input(event):  		
-	if event is InputEventMouseMotion:#and Input.is_action_pressed("mouse_right"):
-		mNode.rotate_y(deg_to_rad(event.relative.x*mouse_sens))
-		hrzn.rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
+	if event is InputEventMouseMotion: #My movement code
+		mNode.rotate_y(deg_to_rad(event.relative.x*mouse_sens)) #moNode, meaning movement Node, ensures that my movement is going the right way (because the way your camera turns is the opposite way your character should move)
+		hrzn.rotate_y(deg_to_rad(-event.relative.x*mouse_sens)) 
 		vert.rotate_x(deg_to_rad(-event.relative.y*0.3))
 		vert.rotation.x = clamp(vert.rotation.x, deg_to_rad(-90.0), deg_to_rad(60.0))
 	if $feet.is_colliding():
 		grounded = 1
 		jumps = 2
-	else: 
-		grounded = 0
+		launch = false
 	if Input.is_action_just_pressed("space"):
-		if jumps > 0:
-			apply_central_impulse(Vector3(0, 1 ,0) * 6 )
-			jumps -=1
-	else:
-		pass
+		if grounded == 1:
+			if jumps == 2:
+				apply_central_impulse(Vector3(0, 1 ,0) * 7 )
+				jumps -=1
+			elif jumps == 1:
+				if linear_velocity.y <= 0.25:
+					apply_central_impulse(Vector3(0, 1 ,0) * 10 )
+					jumps -=1
+			elif jumps == 0:
+				grounded = 0
+
+
+func _on_grapple_controller_launching():
+	launch = true
+	launchWas = true
+
+
+func _on_grapple_controller_retracted():
+	launchWas = false
+
+
+
--- grapplingGun.gd
+++ grapplingGun.gd
@@ -1,18 +1,22 @@
 extends Node3D
-signal launching(hook_target)
+signal launching
 signal retracted
+signal point
 #This whole code will the grappling code
 #I will attempt to make a 3D version of the springJoint that the 2D space has.
 var colliding = false
 @export var RayCast: RayCast3D
-@onready var ray = $"../v/h/s/Camera3D/RayCast3D"
+@onready var ray = $"../v/h/S/Camera3D/RayCast3D"
 @export var stiffness := 50.0
 @export var rest_length := 5.0
 @export var damping := 1.0
 @onready var player = get_parent()
+
 func _ready():
-	pass
+	pass # Replace with function body.
+
 var launched = false
+var hook_target = Vector3.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("shoot") and colliding == true:
@@ -23,18 +27,18 @@ func _process(delta):
 		emit_signal("retracted")
 	if launched == true:
 		grapple_physics(delta)
+
 func retract():
 	launched = false
-var hook_target: Vector3
+
 func launch():
-	hook_target = ray.get_collision_point() 
+	hook_target = ray.get_collision_point()
 	launched = true
-	
-func grapple_physics(_delta):
+
+func grapple_physics(delta):
 	var dir_player_targ = player.global_position.direction_to(hook_target)
 	var dist_player_targ = player.global_position.distance_to(hook_target)
-	
-
+	emit_signal("point")
 	var force = Vector3.ZERO
 	var displacement = dist_player_targ - rest_length  #Springy part. If the distance is more than it should be,
 	#our player will move towards what it should be(rest length). Else, gravity will take care of it.
@@ -46,6 +50,7 @@ func grapple_physics(_delta):
 		var damp = -damping * vel_dot * dir_player_targ
 		damp.y -= 7
 		force = stiffness * displacement * dir_player_targ + damp
+		#print(vel_dot)
 	player.apply_central_force(force)
 func _on_bean_col_t():
 	colliding = true
