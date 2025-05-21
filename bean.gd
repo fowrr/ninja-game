b43d04f6b390868a115358fd639f32a7fff93360
fowrr <smand3539@gmail.com>
2025-05-20 23:38:44 -0400

    got a lot working i think but i cant figure out how to get my swinging based off of an arc

--- bean.gd
+++ bean.gd
@@ -2,15 +2,14 @@ extends RigidBody3D
 signal veloSpeed
 signal colF
 signal colT
-var velocity := 2500.0
+var vMultiplier := 1000.0
 var mouse_sens = 0.3
 @onready var hrzn = $v
 @onready var vert = $v/h
 @onready var mNode = $movementNode
 @onready var velocity_label = $"Control/velocity"
 var grounded = 0
-var max_horizontal_speed := 2.0
-var max_diagonal_speed := 2.82842712475
+var max_horizontal_speed := 3.0
 var jumps = 2
 @onready var rayCast = $v/h/S/Camera3D/RayCast3D
 @onready var h = ($"v/h/S/Camera3D/RayCast3D/rayball").get_surface_override_material(0)
@@ -20,36 +19,57 @@ var jumps = 2
 var colliding = false
 var launch = false
 var launchWas = false
+var inputVector = Vector3.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	linear_damp = 1.0
 	
-
+var rope_dir = Vector3.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var horizontal_velocity = Vector2(linear_velocity.x, linear_velocity.z)
-	#print(rayCast.transform.basis)
-	#Code making sure my velocity is never higher than my max speed.
-	#===============================================================#
-	var current_velocity = linear_velocity # Get the current velocity of the rigid body 
-	#print(current_velocity.length())
-	if linear_velocity.y <= -1.5:
-		gravity_scale = 1
+	
+	#If you're in the air, fall faster.
+	if linear_velocity.y <= -1.0:
+		gravity_scale = 1.5
 	else:
 		gravity_scale = 1
 		
 	
 	#Label for velocity
-	veloSpeed.emit(current_velocity)
-	velocity_label.text = "Velocity: " + str(current_velocity)
+	veloSpeed.emit(linear_velocity)
+	velocity_label.text = "Velocity: " + str(linear_velocity)
+	
+	
+	#Code for my movement
+	#===============================================================#
+	var horizontal_velocity = Vector2(linear_velocity.x, linear_velocity.z)
 	
 	#Gets my movement (A,W,S,D or arrow keys)
 	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	var horizon_basis = mNode.basis #Check for nMode explanation (second line in unhandled input).
-	apply_central_force(Vector3(input.x, 0 ,input.y) * velocity * 1 * delta * horizon_basis)
-	
-	#hi
+	var horizon_basis = mNode.basis #Check for nMode explanation (second line in unhandled input).)
+	apply_central_force(inputVector * vMultiplier * 1 * delta * horizon_basis) #Apply movement
+	if Input.is_action_pressed("shift"):
+		max_horizontal_speed = 9.0
+		vMultiplier = 4000
+	else:
+		max_horizontal_speed = lerp(max_horizontal_speed, 3.0, 0.1)
+		vMultiplier = 2500
+	#print(horizontal_velocity.length())
+	#Limiting velocity:
+	#print(horizon_basis)
+	if launch == true:
+		inputVector = Vector3( 0 , 0 ,input.y)
+	elif launch == false and Input.is_action_pressed("shift"):
+		inputVector = Vector3(input.x, 0 ,input.y)
+		if horizontal_velocity.length() > 9.2:
+			horizontal_velocity = horizontal_velocity.normalized() * max_horizontal_speed
+			linear_velocity = Vector3(horizontal_velocity.x,linear_velocity.y,horizontal_velocity.y)
+	else:
+		inputVector = Vector3(input.x, 0 ,input.y)
+		if horizontal_velocity.length()> 3.1:
+			horizontal_velocity = horizontal_velocity.normalized() * max_horizontal_speed
+			linear_velocity = Vector3(horizontal_velocity.x,linear_velocity.y,horizontal_velocity.y)
 	col()
 
 #This works
@@ -88,7 +108,7 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("space"):
 		if grounded == 1:
 			if jumps == 2:
-				apply_central_impulse(Vector3(0, 1 ,0) * 7 )
+				apply_central_impulse(Vector3(0, 1 ,0) * 15 )
 				jumps -=1
 			elif jumps == 1:
 				if linear_velocity.y <= 0.25:
@@ -108,6 +128,7 @@ func _on_grapple_controller_launching():
 
 func _on_grapple_controller_retracted():
 	launchWas = false
+
 
 
 
